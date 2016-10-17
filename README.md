d16edd5ccd2f2ebd507559fa167e4589b174963d
deleepa <deleepa.p@gmail.com>
2016-10-17 06:45:16 +0530

    - Added supertest and validator packages - Added method to validate user and insert into table - Added readme file

---
+++ README.md
@@ -0,0 +1,24 @@
+## Simple API Server
+
+This project is intended to serve as a starting point for anyone that wants to set up a simple NodeJS API server really fast. It's functionality is similar to a boilerplate. I started this mostly for my reference but I hope this ends up helping more people!
+
+Eventually this I want to have a user login module as well, since that's a really basic feature that's part of any API server application. Authenticated routes would also be included.
+
+## Installation
+
+`git clone https://github.com/deleepa/simple-api.git`
+`cd simple-api`
+`npm install`
+`node app.js`
+
+## Tests
+
+I'm using the mocha test framework for this project.
+
+## Contributors
+
+Contributors are more than welcome. The contribution guidelines aren't strict at all. Just have a look at the CONTRIBUTING.md file :)
+
+## License
+
+MIT.
--- routes/user-api.js
+++ routes/user-api.js
@@ -6,6 +6,8 @@
 //init the required modules
 var express = require('express');
 var log = require('debug-logger')('user-api.js');
+var validator = require('validator');
+var crypto = require('crypto');
 
 //init the express router
 var router = express.Router();
@@ -13,6 +15,8 @@ var router = express.Router();
 //table name that this router is linked to
 const TABLE_NAME = "user";
 
+//salt to be used with cipher
+const CIPHER_SALT = "salt";
 
 module.exports = function Router(db) {
 
@@ -30,12 +34,16 @@ module.exports = function Router(db) {
                    "status": false,
                    "message": err
                });
+               
+               return;
            } 
            else {
                res.status(200).json({
                    "status": true,
                    "message": rows
                });
+               
+               return;
            }
         });
     });
@@ -43,6 +51,53 @@ module.exports = function Router(db) {
     router.post('/', function (req, res) {
         console.log('posting to user table');
         console.log(req.body);
+        
+        //validate email address
+        if(!validator.isEmail(req.body.email)) {
+            res.status(500).json({
+                "status": false,
+                "message": "The email provided is not valid."
+            });
+            
+            return;
+        }
+        //validate password length
+        if(req.body.password.length < 8) {
+            res.status(500).json({
+                "status": false,
+                "message": "The password must be at least 8 characters."
+            });
+            
+            return;
+        }
+        
+        var cipher = crypto.createCipher('aes192', CIPHER_SALT);
+        var encryptedPass = cipher.update(eq.body.password, 'utf-8', 'hex');
+        encryptedPass += cipher.final('hex');
+        
+        var query = "INSERT INTO " + TABLE_NAME + " (email, password, package_id) " + 
+                    "VALUES ('" + req.body.email + "', '" + req.body.password + "', 1)";
+                    
+        console.log(query);
+        
+        db.query(query, function(err, rows) {
+           if(err) {
+               res.status(500).json({
+                   "status": false,
+                   "message": err
+               });
+               
+               return;
+           } 
+           else {
+               res.status(200).json({
+                   "status": true,
+                   "message": rows
+               });
+               
+               return;
+           }
+        });
     });
     
     return router;
