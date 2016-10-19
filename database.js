2e1f0975551ba3b266589cdc4f3eec058f7feacc
dhwanilvyas <dhwanilvyas@outlook.com>
2016-10-19 11:33:37 +0530

    Added .env to gitignore, created .env.example, updated database config file to take default values

--- database.js
+++ database.js
@@ -3,14 +3,14 @@ var log = require('debug-logger')('database.js');
 
 module.exports = function Database() {
     var config = {
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        database: process.env.DB_NAME
+        host: process.env.DB_HOST || 'localhost',
+        user: process.env.DB_USER || 'root',
+        database: process.env.DB_NAME || 'simple-api-server'
     };
 
     //check if the user is using a password or not
     if(typeof connection !== undefined) {
-        config.password = process.env.DB_PASSWORD;
+        config.password = process.env.DB_PASSWORD || '';
     }
 
     //create the connection with the database
