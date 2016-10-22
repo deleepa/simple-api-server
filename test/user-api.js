a204cd888fa2ab3c12a1741593b904c0d921d577
Singhabahu, Chamara <mails.chamara@gmail.com>
2016-10-22 13:11:26 +0530

    Add unit test for PUT route

--- test/user-api.js
+++ test/user-api.js
@@ -43,6 +43,26 @@ describe('Create user', function() {
     });
 });
 
+describe('Update user', function() {
+    var user = { password : 'password_new', email : 'marcus@marcus.com'}; 
+
+    it('should return the updated user', function(done) {
+        server
+            .put('/api/users/marcus@marcus.com')
+            .send(user)
+            .expect(200)
+            .end(function(err, res) {
+                if(err) {
+                    throw err;
+                }
+                else {
+                    res.body.message[0].should.have.property('email');
+                }
+                done();
+            });
+    });
+});
+
 describe('Delete user', function() {
 
     it('delete the user stated', function(done) {
